546e24d499af816d8bdedd38765f781a9522097c
arriqaaq <arriqaaq@yahoo.com>
2015-09-12 00:05:29 +0530

    2 try home

--- speak.py
+++ speak.py
@@ -1,7 +1,16 @@
 from flask import Flask, request, Response
 import plivo, plivoxml
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Call
+engine=create_engine('sqlite:///call.db')
+Base.metadata.bind=engine
+DBSession=sessionmaker(bind=engine)
+session=DBSession()
+Yes=1
+No=0
 app = Flask(__name__)
+auth_id = "MANDCZZTUWZDUWNTBHMZ"
 
 @app.route("/forward/", methods=['GET','POST'])
 def forward():
@@ -12,21 +21,29 @@ def forward():
     # we'll use this as the Caller ID when we forward the call.
 
     from_number = request.args.get('From')
-
+    call_uuid = request.args.get('CallUUID')
+    call_status = request.args.get('CallStatus')
     # The number you would like to forward the call to.
-
     forwarding_number = "919092937238"
 
-    params = {
-        'callerId': from_number # The phone number to be used as the caller id. It can be set to the from_number or any custom number.
-    }
+    checkcalls=session.query(Call).filter_by(busy=Yes).one()
 
-    response = plivoxml.Response()
 
-    d = response.addDial(**params)
-    d.addNumber(forwarding_number)
-    print response.to_xml()
-    return Response(str(response), mimetype='text/xml')
+    if checkcalls:
+        pass
+    else:
+        print "hello there"
+        newCall=Call(name=call_uuid,status=call_status,busy=Yes)
+        session.add(newCall)
+        session.commit()
+        params = {
+            'callerId': from_number # The phone number to be used as the caller id. It can be set to the from_number or any custom number.
+        }
+        response = plivoxml.Response()
+        d = response.addDial(**params)
+        d.addNumber(forwarding_number)
+        print response.to_xml()
+        return Response(str(response), mimetype='text/xml')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
